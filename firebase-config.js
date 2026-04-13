c4f797fd34599289c556f73fcdab848e0a0e12c8
joshua <joshuajgomez@gmail.com>
2026-04-13 22:29:11 +0200

    Extract firebase config to new file
    Signed-off-by: joshua <joshuajgomez@gmail.com>

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB77chZqaSxaoAZXzHGqhXXeWnrczgNN4Q",
+  authDomain: "rocktv-b1cf5.firebaseapp.com",
+  projectId: "rocktv-b1cf5",
+  storageBucket: "rocktv-b1cf5.firebasestorage.app",
+  messagingSenderId: "653593189256",
+  appId: "1:653593189256:web:9a4e8ba64d02d51719783d",
+  measurementId: "G-0JL5M9V0KJ"
+};
